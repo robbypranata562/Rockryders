f548a39e802a4c2d0ec4889f3896387db3f0ebe9
robby <robbypranata562@gmail.com>
2019-08-31 01:37:50 +0800

    Update home.php
    And Where Is Status = 1

--- admin/home.php
+++ admin/home.php
@@ -25,10 +25,17 @@
         if ($jabatan=='Super Administrator' or $jabatan=='admin')
         { ?>
           <div class="col-lg-6 col-xs-12">
-            <div class="small-box bg-yellow">
+            <div class="small-box bg-aqua">
               <div class="inner">
                 <?php
-                  $sql_select="SELECT COUNT(*) FROM transaction where DeletedDate is null and DeletedBy is null";
+                  $sql_select="SELECT 
+                    COUNT(*) 
+                  FROM 
+                    transactionGudang 
+                  where 
+                    DeletedDate is null 
+                    and DeletedBy is null
+                    and isConfirm = 1";
                   $exe_select=mysqli_query($koneksi,$sql_select);
                   $row=mysqli_fetch_row($exe_select);
                 ?>
@@ -41,167 +48,170 @@
               <a href="history_penjualan.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
             </div>
           </div> 
-
+          <div class="col-lg-6 col-xs-12">
+            <div class="small-box bg-yellow">
+              <div class="inner">
+                <?php
+                  $sql_select="SELECT 
+                    COUNT(*) 
+                  FROM 
+                    transaction 
+                  where 
+                    DeletedDate is null 
+                    and DeletedBy is null
+                    and isConfirm = 1";
+                  $exe_select=mysqli_query($koneksi,$sql_select);
+                  $row=mysqli_fetch_row($exe_select);
+                ?>
+                  <h3><?php echo $row[0];?></h3>
+                  <p>Transaksi</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-bag"></i>
+              </div>
+              <a href="history_penjualan.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+          </div> 
           <div class="col-lg-6 col-xs-12">
             <div class="small-box bg-aqua">
               <div class="inner">
                 <?php
-                  $sql_select="SELECT COUNT(*) FROM transactionGudang where DeletedDate is null and DeletedBy is null";
-                  $exe_select=mysqli_query($koneksi,$sql_select);
-                  $row=mysqli_fetch_row($exe_select);
+                  $jum_notif="SELECT jum_minimal from notif where id=1";
+                  $exe_jum=mysqli_query($koneksi,$jum_notif);
+                  while($b=mysqli_fetch_assoc($exe_jum))
+                  {
+                    $ab=$b['jum_minimal'];
+                  }
+                  $sql_selectbrg="SELECT COUNT(SmallQty) FROM itemGudang where MinStock <= '$ab'";
+                  $exe_brg=mysqli_query($koneksi,$sql_selectbrg);
+                  $row1=mysqli_fetch_row($exe_brg);
                 ?>
-                  <h3><?php echo $row[0];?></h3>
-                  <p>Transaksi</p>
+                <h3><?php echo $row1[0];?></h3>
+                <p>Periksa Stok Barang Gudang</p>
               </div>
               <div class="icon">
-                <i class="ion ion-bag"></i>
+                <i class="ion ion-stats-bars"></i>
               </div>
-              <a href="history_penjualan.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+              <a href="toko_tampil.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
             </div>
-          </div> 
-
-
-		<div class="col-lg-6 col-xs-12">
-      <div class="small-box bg-yellow">
-        <div class="inner">
-          <?php
-            $jum_notif="SELECT jum_minimal from notif where id=1";
-            $exe_jum=mysqli_query($koneksi,$jum_notif);
-            while($b=mysqli_fetch_assoc($exe_jum))
-            {
-              $ab=$b['jum_minimal'];
-            }
-            $sql_selectbrg="SELECT COUNT(SmallQty) FROM item where MinStock <= '$ab'";
-            $exe_brg=mysqli_query($koneksi,$sql_selectbrg);
-            $row1=mysqli_fetch_row($exe_brg);
-          ?>
-          <h3><?php echo $row1[0];?></h3>
-          <p>Periksa Stok Barang Toko</p>
-        </div>
-        <div class="icon">
-          <i class="ion ion-stats-bars"></i>
-        </div>
-        <a href="toko_tampil.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-      </div>
-    </div>
-
-    <div class="col-lg-6 col-xs-12">
-      <div class="small-box bg-aqua">
-        <div class="inner">
-          <?php
-            $jum_notif="SELECT jum_minimal from notif where id=1";
-            $exe_jum=mysqli_query($koneksi,$jum_notif);
-            while($b=mysqli_fetch_assoc($exe_jum))
-            {
-              $ab=$b['jum_minimal'];
-            }
-            $sql_selectbrg="SELECT COUNT(SmallQty) FROM itemGudang where MinStock <= '$ab'";
-            $exe_brg=mysqli_query($koneksi,$sql_selectbrg);
-            $row1=mysqli_fetch_row($exe_brg);
-          ?>
-          <h3><?php echo $row1[0];?></h3>
-          <p>Periksa Stok Barang Gudang</p>
-        </div>
-        <div class="icon">
-          <i class="ion ion-stats-bars"></i>
-        </div>
-        <a href="toko_tampil.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-      </div>
-    </div>
-
-    <div class="col-lg-6 col-xs-12">
-      <div class="small-box bg-yellow">
-        <div class="inner">
-          <?php
-            $sql_selectbrg="SELECT
-            IFNULL(sum(b.Qty), 0) AS Total
-          FROM
-            `transaction` AS a
-          LEFT JOIN transactiondetail AS b ON a.Id = b.TransactionId
-          WHERE
-            a.DeletedBy IS NULL
-          AND a.DeletedDate IS NULL";
-            $exe_brg=mysqli_query($koneksi,$sql_selectbrg);
-            $row1=mysqli_fetch_row($exe_brg);
-          ?>
-          <h3><?php echo $row1[0] ." Pcs";?></h3>
-          <p>Kaos Terjual Sampai Hari Ini (Toko)</p>
-        </div>
-        <div class="icon">
-          <i class="ion ion-stats-bars"></i>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-lg-6 col-xs-12">
-      <div class="small-box bg-aqua">
-        <div class="inner">
-          <?php
-            $sql_selectbrg="SELECT
-            IFNULL(sum(b.Qty), 0) AS Total
-          FROM
-            `transactionGudang` AS a
-          LEFT JOIN transactiondetailgudang AS b ON a.Id = b.TransactionId
-          WHERE
-            a.DeletedBy IS NULL
-          AND a.DeletedDate IS NULL";
-            $exe_brg=mysqli_query($koneksi,$sql_selectbrg);
-            $row1=mysqli_fetch_row($exe_brg);
-          ?>
-          <h3><?php echo $row1[0] ." Pcs";?></h3>
-          <p>Kaos Terjual Sampai Hari Ini (Gudang)</p>
-        </div>
-        <div class="icon">
-          <i class="ion ion-stats-bars"></i>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-lg-6 col-xs-12">
-      <div class="small-box bg-yellow">
-        <div class="inner">
-          <?php
-            $sql_selectbrg="SELECT
-            Format(IFNULL(sum(a.TotalPrice), 0),2) AS Total
-          FROM
-            `transaction` AS a
-          WHERE
-            a.DeletedBy IS NULL
-          AND a.DeletedDate IS NULL";
-            $exe_brg=mysqli_query($koneksi,$sql_selectbrg);
-            $row1=mysqli_fetch_row($exe_brg);
-          ?>
-          <h3><?php echo $row1[0];?></h3>
-          <p>Total Pendapatan Sampai Hari Ini (Gudang)</p>
-        </div>
-        <div class="icon">
-          <i class="ion ion-stats-bars"></i>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-lg-6 col-xs-12">
-      <div class="small-box bg-aqua">
-        <div class="inner">
-          <?php
-            $sql_selectbrg="SELECT
-            Format(IFNULL(sum(a.TotalPrice), 0),2) AS Total
-          FROM
-            `transactiongudang` AS a
-          WHERE
-            a.DeletedBy IS NULL
-          AND a.DeletedDate IS NULL";
-            $exe_brg=mysqli_query($koneksi,$sql_selectbrg);
-            $row1=mysqli_fetch_row($exe_brg);
-          ?>
-          <h3><?php echo $row1[0];?></h3>
-          <p>Total Pendapatan Sampai Hari Ini (Gudang)</p>
-        </div>
-        <div class="icon">
-          <i class="ion ion-stats-bars"></i>
-        </div>
-      </div>
-    </div>
+          </div>
+          <div class="col-lg-6 col-xs-12">
+            <div class="small-box bg-yellow">
+              <div class="inner">
+                <?php
+                  $jum_notif="SELECT jum_minimal from notif where id=1";
+                  $exe_jum=mysqli_query($koneksi,$jum_notif);
+                  while($b=mysqli_fetch_assoc($exe_jum))
+                  {
+                    $ab=$b['jum_minimal'];
+                  }
+                  $sql_selectbrg="SELECT COUNT(SmallQty) FROM item where MinStock <= '$ab'";
+                  $exe_brg=mysqli_query($koneksi,$sql_selectbrg);
+                  $row1=mysqli_fetch_row($exe_brg);
+                ?>
+                <h3><?php echo $row1[0];?></h3>
+                <p>Periksa Stok Barang Toko</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-stats-bars"></i>
+              </div>
+              <a href="toko_tampil.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <div class="col-lg-6 col-xs-12">
+            <div class="small-box bg-aqua">
+              <div class="inner">
+                <?php
+                  $sql_selectbrg="SELECT
+                  IFNULL(sum(b.Qty), 0) AS Total
+                FROM
+                  `transactionGudang` AS a
+                LEFT JOIN transactiondetailgudang AS b ON a.Id = b.TransactionId
+                WHERE
+                  a.DeletedBy IS NULL
+                  AND a.DeletedDate IS NULL
+                  And a.isConfirm = 1";
+                  $exe_brg=mysqli_query($koneksi,$sql_selectbrg);
+                  $row1=mysqli_fetch_row($exe_brg);
+                ?>
+                <h3><?php echo $row1[0] ." Pcs";?></h3>
+                <p>Kaos Terjual Sampai Hari Ini (Gudang)</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-stats-bars"></i>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-6 col-xs-12">
+            <div class="small-box bg-yellow">
+              <div class="inner">
+                <?php
+                  $sql_selectbrg="SELECT
+                  IFNULL(sum(b.Qty), 0) AS Total
+                FROM
+                  `transaction` AS a
+                LEFT JOIN transactiondetail AS b ON a.Id = b.TransactionId
+                WHERE
+                  a.DeletedBy IS NULL
+                  AND a.DeletedDate IS NULL
+                  AND a.IsConfirm = 1";
+                  $exe_brg=mysqli_query($koneksi,$sql_selectbrg);
+                  $row1=mysqli_fetch_row($exe_brg);
+                ?>
+                <h3><?php echo $row1[0] ." Pcs";?></h3>
+                <p>Kaos Terjual Sampai Hari Ini (Toko)</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-stats-bars"></i>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-6 col-xs-12">
+            <div class="small-box bg-aqua">
+              <div class="inner">
+                <?php
+                  $sql_selectbrg="SELECT
+                  Format(IFNULL(sum(a.TotalPrice), 0),2) AS Total
+                FROM
+                  `transactiongudang` AS a
+                WHERE
+                  a.DeletedBy IS NULL
+                  AND a.DeletedDate IS NULL
+                  And a.isConfirm = 1";
+                  $exe_brg=mysqli_query($koneksi,$sql_selectbrg);
+                  $row1=mysqli_fetch_row($exe_brg);
+                ?>
+                <h3><?php echo $row1[0];?></h3>
+                <p>Total Pendapatan Sampai Hari Ini (Gudang)</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-stats-bars"></i>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-6 col-xs-12">
+            <div class="small-box bg-yellow">
+              <div class="inner">
+                <?php
+                  $sql_selectbrg="SELECT
+                  Format(IFNULL(sum(a.TotalPrice), 0),2) AS Total
+                FROM
+                  `transaction` AS a
+                WHERE
+                  a.DeletedBy IS NULL
+                  AND a.DeletedDate IS NULL
+                  And a.IsConfirm = 1";
+                  $exe_brg=mysqli_query($koneksi,$sql_selectbrg);
+                  $row1=mysqli_fetch_row($exe_brg);
+                ?>
+                <h3><?php echo $row1[0];?></h3>
+                <p>Total Pendapatan Sampai Hari Ini (Toko)</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-stats-bars"></i>
+              </div>
+            </div>
+          </div>
 		<?php } ?>
   </section>
 </div>
