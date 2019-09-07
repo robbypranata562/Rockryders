1291a6825aa5e56db8277317044a9e6ee637ed48
Cakra Wardhana <wardhanacakra@gmail.com>
2019-09-07 09:56:23 +0700

    push

--- admin/koneksi.php
+++ admin/koneksi.php
@@ -10,7 +10,7 @@ $db_name = "rockrydersDB";
 // $db_name = "rockrydersDB";
 
 $koneksi = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
-
+ 
 if(mysqli_connect_error()){
 	echo 'Gagal melakukan koneksi ke Database : '.mysqli_connect_error();
 }
