ae70d97b4f2d286b25045da531c98bcff1de5b18
robby <robbypranata562@gmail.com>
2019-09-16 01:42:10 +0800

    Update ActBatchCheckStockByColorAndSize.php
    Push Change Left Join For New Flow

--- admin/ActBatchCheckStockByColorAndSize.php
+++ admin/ActBatchCheckStockByColorAndSize.php
@@ -18,7 +18,6 @@
             and a.DeletedBy is Null
             and a.DeletedDate is null
         ";
-        // die($SQLCheckStok);
         $result=mysqli_query($koneksi,$SQLCheckStok);
         if(mysqli_num_rows($result) > 0 )
         {
@@ -44,7 +43,7 @@
               INNER JOIN size AS c ON a.Size = c.`Code`
               WHERE
               	1 = 1
-                and a.Color = '".$key[0]."'
+                and b.Name = '".$key[0]."'
                 and a.Size  = '".$key[1]."'
                 and a.DeletedBy is Null
                 and a.DeletedDate is null
