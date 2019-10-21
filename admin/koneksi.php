963c4f67da7b9c8a169768bb6af84543e2f40f2b
robby <robbypranata562@gmail.com>
2019-10-21 16:39:44 +0800

    Update koneksi.php
    Update Koneksi.Php

--- admin/koneksi.php
+++ admin/koneksi.php
@@ -4,15 +4,15 @@
 // $db_pass = "@b6F6n7c";
 // $db_name = "rockrydersDB";
 
-$db_host = "localhost";
-$db_user = "root";
-$db_pass = "";
-$db_name = "kaospolo_rockrydersdb";
+// $db_host = "localhost";
+// $db_user = "root";
+// $db_pass = "";
+// $db_name = "kaospolo_rockrydersdb";
 
-// $db_host = "localhost:3306";
-// $db_user = "kaospolo_dbrock";
-// $db_pass = "5r*j1yP4";
-// $db_name = "kaospolo_rockrydersDB";
+$db_host = "localhost:3306";
+$db_user = "kaospolo_dbrock";
+$db_pass = "5r*j1yP4";
+$db_name = "kaospolo_rockrydersDB";
 
 $koneksi = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
