b2dee1742552745cd4d612f429debdd658cff987
robby <robbypranata562@gmail.com>
2019-10-23 10:20:23 +0800

    Update DeliveryOrderPrintNew.php

--- admin/DeliveryOrderPrintNew.php
+++ admin/DeliveryOrderPrintNew.php
@@ -109,7 +109,7 @@ include "koneksi.php";
     <br/>
     <br/>
     <div class="row">
-        <div class="col-print-12 table-responsive">
+        <div class="col-md-12 table-responsive">
             <table class="table table-striped">
                 <thead>
                     <tr>
